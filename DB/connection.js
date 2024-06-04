c2057aa90aa0b79bdca2dcd468a24b1c301adbf9
yehia <yhia.themora@gmail.com>
2024-06-04 03:32:23 +0300

    hamada

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const result = await mongoose.connect(process.env.DB_LOCAL);
+    const result = await mongoose.connect(process.env.mongodb, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
     return console.log(`DB connected successfully`);
   } catch (err) {
     return console.log(`Failed to connect on DB ${err}`);
