91e13e91495e6511d12b4335608d8b8969815408
unknown <yehiaaboulela03@gmail.com>
2024-06-15 18:52:34 -0700

    move to mac

--- DB/connection.js
+++ DB/connection.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const result = await mongoose.connect(process.env.mongodb, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    const result = await mongoose.connect(process.env.DB_LOCAL);
     return console.log(`DB connected successfully`);
   } catch (err) {
     return console.log(`Failed to connect on DB ${err}`);
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import * as dotenv from "dotenv";
-dotenv.config();
 
 import express from "express";
 import initApp from "./src/app.router.js";
 import sendEmail from "./src/utils/email.js";
 const app = express();
-const port = process.env.PORT || 10000;
+dotenv.config();
+const port = 5000;
 
 initApp(app, express);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
